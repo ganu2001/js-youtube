a1cd1ae7dcfff09736abcf54ec65b22034b89a16
ganu2001 <ganeshphutane2001@gmail.com>
2024-10-16 12:12:55 +0530

    first commit

---
+++ 03_conversion.js
@@ -0,0 +1,14 @@
+let a = "asd123"
+
+
+console.log(typeof a)
+
+let b = Number(a)
+console.log(typeof b )
+
+console.log( b )  // jar apan mix String convert kela no madhye tr te convert tr hote pan techi value NaN yete
+
+
+// "22" = 22
+// "22asd" = NaN
+// true = 1 , false = 0 
