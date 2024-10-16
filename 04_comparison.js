77fed285282cd504e9fd68467be6c59f93a3627e
ganu2001 <ganeshphutane2001@gmail.com>
2024-10-16 12:38:36 +0530

    first commit

---
+++ 04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);   //null convert into a 0 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2); // ethe he phakta value nahi tr data type pan check karate 
