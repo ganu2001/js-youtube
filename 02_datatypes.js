9ac37e2c630e4e484e09dbcc0c408fa25e4a5e34
ganu2001 <ganeshphutane2001@gmail.com>
2024-10-16 12:01:58 +0530

    first commit

---
+++ 02_datatypes.js
@@ -0,0 +1,17 @@
+// premative data types 
+
+// number = 2 to power of 53
+// BigInt 
+// String 
+// Boolean = true /false 
+// null = Standalone value 
+// undefined = ata value define nah zala 
+// Symbol = for a unique value 
+
+
+
+// Objects 
+
+console.log(typeof "gnesh")
+console.log(typeof undefined)
+console.log(typeof null)
